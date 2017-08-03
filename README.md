9955723809339226bc803ec392aa744c0df4db10
Dave Boyle <dave.boyle@investorsgroup.com>
2017-08-03 16:53:23 -0400

    [master] - Updated README to explain Mavensmate setup, renamed name_value_item component and added variant.

--- README.md
+++ README.md
@@ -6,6 +6,31 @@ Contents:
 =========
 
 1) Aura Components
+
 2) Static Resources
+
 3) cp-patternlib - A static version of the client portal specific styling, contains overrides for the slds, and custom igcp styles, as well as element styling.
 4) slds - A customized static version of the Salesforce Lightning Design System
+
+Environment Setup
+=================
+
+1) Download and install Mavensmate(MM) desktop: http://mavensmate.com/
+
+2) Inside Mavensmate(MM) open settings and set workspace path to the location of your Mavensmate workspace (Just create a folder 'mm_workspace' if you don't have a pre-existing workspace).
+
+3) In MM click on the folder icon and select New Project, this will prompt you to log into your Salesforce environment. 
+
+4) Under project name, name your project 'igcp-portal' and create project, this will auto generate a 'igcp-portal' directory in your MM workspace.
+
+5) Go inside MM workspace in explorer/finder and rename generated directory to 'igcp-mm'.
+
+6) Inside MM workspace clone igcp-portal repo: https://github.com/igfed/igcp-portal.git
+
+7) Go inside 'igcp-mm' and copy the 'config' directory and any IDE settings file (ex. igcp-portal.sublime-workspace, igcp-portal.sublime-project) over to 'igcp-portal' 
+
+8) Delete 'igcp-mm'
+
+9) Inside MM click on Monitor icon in the left sidebar and launch your preferred IDE.
+
+10) Everytime you save a file, MM should now sync the changes with the SF environment
--- src/aura/CP_Cmp_Name_Value_List/CP_Cmp_Name_Value_ListController.js
+++ src/aura/CP_Cmp_Name_Value_List/CP_Cmp_Name_Value_ListController.js
@@ -2,35 +2,39 @@
 	doInit: function(cmp, evt, hlpr) {
 
 		var 
-			values = cmp.get("v.values")['values'];
-			termClass = cmp.get("v.termClass"),
-			descriptionClass = cmp.get("v.descriptionClass");
+			values = cmp.get("v.values")['values'],
+			variant = cmp.get("v.variant"),
+			componentName = "";	
+
+		if(variant === "a") {
+			componentName = "c:CP_Cmp_Name_Value_Item_A";
+		} else if (variant === "b") {
+			componentName = "c:CP_Cmp_Name_Value_Item_B";
+		} else {
+			console.warn('CP_Cmp_Name_Value_List: variant ' + variant + ' unrecognized');
+		}
 
 		values.forEach(function(item, i) {
-			for (var key in item) {
 
-				$A.createComponent(
-					"c:CP_Cmp_Name_Value_Item", {
-						"label": hlpr.convertToSpaces(key),
-						"description": item[key],
-						"termClass" : termClass,
-						"descriptionClass" : descriptionClass
-					},
-					function(nameValueItem, status, errorMessage) {
-						if (status === "SUCCESS") {
-							var body = cmp.get("v.body");
-							body.push(nameValueItem);
-							cmp.set("v.body", body);
-						} else if (status === "INCOMPLETE") {
-							console.log("No response from server or client is offline.")
-							// Show offline error
-						} else if (status === "ERROR") {
-							console.log("Error: " + errorMessage);
-							// Show error message
-						}
+			$A.createComponent(
+				componentName, {
+					"label": item.label,
+					"description": item.detail
+				},
+				function(nameValueItem, status, errorMessage) {
+					if (status === "SUCCESS") {
+						var body = cmp.get("v.body");
+						body.push(nameValueItem);
+						cmp.set("v.body", body);
+					} else if (status === "INCOMPLETE") {
+						console.log("No response from server or client is offline.")
+						// Show offline error
+					} else if (status === "ERROR") {
+						console.log("Error: " + errorMessage);
+						// Show error message
 					}
-				);
-			}
+				}
+			);
 		});
 	}
 })
