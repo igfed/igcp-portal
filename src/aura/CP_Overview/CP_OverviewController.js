4b7b84d12a528a30c1cafd18a74a26bf497ebc3e
David Boyle <spektraldevelopment@davids-mbp.mackenziefinancial.bz>
2017-10-03 13:33:25 -0400

    [master] - Added client name to Overview page.

--- src/aura/CP_Overview/CP_OverviewController.js
+++ src/aura/CP_Overview/CP_OverviewController.js
@@ -1,5 +1,18 @@
 ({
 	onInit : function(cmp, evt, hlpr) {
 	
+		var 
+			services = cmp.find("CP_Services");
+
+		services.getClientFirstName(
+			cmp,
+			function(name){
+				cmp.set("v.clientName", (" " + name));
+			},
+			function(error) {
+				console.error("Overview: getClientFirstName");
+				console.error(error);
+			}
+		);
 	}
 })
--- src/aura/CP_Services/CP_ServicesController.js
+++ src/aura/CP_Services/CP_ServicesController.js
@@ -255,6 +255,134 @@
 
 		}
 	},
+	onGetClientFirstName: function(cmp, evt, hlpr) {
+		var params = evt.getParam("arguments");
+		if (params) {
+			var 
+				action,
+				component = params.component;
+
+			try {
+
+				action = component.get("c.getUserFirstName");
+
+				action.setCallback(this, function(response) {
+
+					var state = response.getState();
+
+					if (state === "SUCCESS") {
+						// Alert the user with the value returned 
+						// from the server
+
+						if (response !== null) {
+							params.successCB(response.returnValue);
+						} else {
+							params.errorCB({
+								"payload": "No first name was returned",
+								"type": "no-name"
+							});
+						}
+
+						// You would typically fire a event here to trigger 
+						// client-side notification that the server-side 
+						// action is complete
+
+					} else if (state === "INCOMPLETE") {
+						// do something
+						params.errorCB({
+							"payload": "Incomplete",
+							"type": "server-side-error"
+						});
+					} else if (state === "ERROR") {
+						var errors = response.getError();
+
+						if (errors) {
+							if (errors) {
+								params.errorCB({
+									"payload": errors,
+									"type": "server-side-error"
+								});
+							}
+						} else {
+							params.errorCB({
+								"payload": "Unknown error",
+								"type": "server-side-error"
+							})
+						}
+					}
+				});
+
+				$A.enqueueAction(action);
+			} catch(err){
+				console.error("CP_Services: onGetClientFirstName: controller not found, make sure it is attached to parent component.");
+				console.log(err);
+			}
+		}
+	},
+	onGetClientFullName: function(cmp, evt, hlpr) {
+		var params = evt.getParam("arguments");
+		if (params) {
+			var 
+				action,
+				component = params.component;
+
+			try {
+
+				action = component.get("c.getUserName");
+
+				action.setCallback(this, function(response) {
+
+					var state = response.getState();
+
+					if (state === "SUCCESS") {
+						// Alert the user with the value returned 
+						// from the server
+
+						if (response !== null) {
+							params.successCB(response.returnValue);
+						} else {
+							params.errorCB({
+								"payload": "No first name was returned",
+								"type": "no-name"
+							});
+						}
+
+						// You would typically fire a event here to trigger 
+						// client-side notification that the server-side 
+						// action is complete
+
+					} else if (state === "INCOMPLETE") {
+						// do something
+						params.errorCB({
+							"payload": "Incomplete",
+							"type": "server-side-error"
+						});
+					} else if (state === "ERROR") {
+						var errors = response.getError();
+
+						if (errors) {
+							if (errors) {
+								params.errorCB({
+									"payload": errors,
+									"type": "server-side-error"
+								});
+							}
+						} else {
+							params.errorCB({
+								"payload": "Unknown error",
+								"type": "server-side-error"
+							})
+						}
+					}
+				});
+				
+				$A.enqueueAction(action);
+			} catch(err){
+				console.error("CP_Services: onGetClientFullName: controller not found, make sure it is attached to parent component.");
+				console.log(err);
+			}
+		}
+	},
 	onGetInvestmentsPreview: function(cmp, evt, hlpr) {
 		var params = evt.getParam("arguments");
 		if (params) {
