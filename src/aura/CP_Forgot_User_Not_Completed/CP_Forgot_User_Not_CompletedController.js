29887ef447d5399ea4ae778f52362f94d97c82d6
David Boyle <spektraldevelopment@davids-mbp.mackenziefinancial.bz>
2017-10-04 11:51:50 -0400

    [master] - Switched all login redirects to using utils.gotoLogin method.

--- src/aura/CP_Forgot_User_Not_Completed/CP_Forgot_User_Not_CompletedController.js
+++ src/aura/CP_Forgot_User_Not_Completed/CP_Forgot_User_Not_CompletedController.js
@@ -1,8 +1,6 @@
 ({
 	gotoLogin: function(cmp, evt, hlpr) {
-		$A.get("e.force:navigateToURL").setParams({
-			"url": "https://fitrp3-isam.investorsgroup.com/",
-			"isredirect" : "true"
-		}).fire();
+		var utils = cmp.find("CP_Utils");
+		utils.gotoLogin();
 	}
 })
