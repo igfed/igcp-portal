72eefe0086a2af908c4e9df852388d6d6e2e446d
Dennis Erny <erny.dennis@gmail.com>
2017-10-12 09:48:28 -0400

    DTM controller updates

--- resource-bundles/igcpresources.resource/igcp-resources/scripts/dtm-controller.js
+++ resource-bundles/igcpresources.resource/igcp-resources/scripts/dtm-controller.js
@@ -4,62 +4,80 @@
   window.digitalData.page = {};
   window.digitalData.page.pageInfo = {};
 
-  var event = {},
-    $this,
+  var $this,
     adobeLoader,
     $component,
     $container;
+
+  // Event object that will be pushed into events array
+  var event = {};
   event.component = {};
   event.container = {};
   event.template = {};
+  event.error = {};
+  event.form = {}
+
+  // Handler for Direct calls
+  window.aaDirectCall = (function () {
+
+    function parse(dcname, data) {
+      console.log('parse direct calls');
+
+      _constructEventObj();
+
+    }
+    return {
+      parse: parse
+    };
+  })();
 
 
   function init() {
+
     // Capture page data
-    window.digitalData.pageInstanceID = getPageName() + window.location.hostname + "",
-    window.digitalData.page.pageInfo.pageName = getPageName(),
-    window.digitalData.page.pageInfo.language = getPageLanguage(),
-    window.digitalData.page.pageInfo.server = window.location.href,
-    window.digitalData.page.pageInfo.timezone = new Date().getTimezoneOffset() / 60
-      
+    window.digitalData.pageInstanceID = _getPageName() + window.location.hostname + "";
+    window.digitalData.page.pageInfo.pageName = _getPageName();
+    window.digitalData.page.pageInfo.language = _getPageLanguage();
+    window.digitalData.page.pageInfo.server = window.location.href;
+    window.digitalData.page.pageInfo.timezone = new Date().getTimezoneOffset() / 60;
+
 
     console.log(window.digitalData);
 
     // Only fire this in Salesforce
-    adobeBottomLoader();
+    _adobeScriptLoader();
 
-    // Register click event handler
+    // Register click event handlers
     $('.aa-click').on('click', function (e) {
       e.preventDefault();
       event.type = 'click';
-      constructEventObj($(this));
+      _constructEventObj($(this));
     });
 
-    // Register hover event handler
+    // Register hover event handlers
     $('.aa-hover').on('click', function () {
       event.type = 'hover';
-      constructEventObj($(this));
+      _constructEventObj($(this));
     });
   }
 
-  function getPageName() {
+  function _getPageName() {
     return document.title;
   }
 
-  function getPageLanguage() {
+  function _getPageLanguage() {
     var lang;
 
     if (window.location.href.indexOf('groupe') > 0 || window.location.href.indexOf('/fr/') > 0) {
-      lang= 'fr'
+      lang = 'fr'
     } else {
       lang = 'en'
     }
 
     return lang;
-
   }
 
-  function adobeBottomLoader() {
+  function _adobeScriptLoader() {
     adobeLoader = setInterval(function () {
       if (window._satellite) {
         window._satellite.pageBottom();
@@ -68,61 +86,73 @@
     }, 500);
   }
 
-  function constructEventObj($this) {
-    var topics;
-    var tags = [];
-    var keyValue;
+  function _constructEventObj($this) {
 
-    event.container.topics = [];
-    event.container.tags = [];
+    if (event.type === 'click' || event.type === 'hover') {
+      var topics;
+      var tags = [];
+      var keyValue;
 
-    // Capture trigger data
-    event.goal = $this.data('aa-goal');
-    event.label = $this.data('aa-link-label');
-    event.parentID = $this.data('aa-parent');
+      event.container.topics = [];
+      event.container.tags = [];
 
-    // Is it a nested event or one connected via an ID?
-    if (event.parentID) {
-      $component = $('body').find('[data-aa-id="' + event.parentID + '"]');
-      $container = $component.parents('.ig-analytics-container');
-    } else {
-      $component = $this.parents('.ig-analytics-component');
-      $container = $this.parents('.ig-analytics-container');
+      // Capture trigger data
+      event.goal = $this.data('aa-goal');
+      event.label = $this.data('aa-link-label');
+      event.parentID = $this.data('aa-parent');
+
+      // Is it a nested event or one connected via an ID?
+      if (event.parentID) {
+        $component = $('body').find('[data-aa-id="' + event.parentID + '"]');
+        $container = $component.parents('.ig-analytics-container');
+      } else {
+        $component = $this.parents('.ig-analytics-component');
+        $container = $this.parents('.ig-analytics-container');
+      }
+
+      // Capture component data
+      if ($component.length > 0) {
+        event.component.id = $component.data('aa-id');
+        event.component.name = $component.data('aa-name');
+        event.component.location = $component.data('aa-location');
+        if (!event.parentID) {
+          event.parentID = event.component.id;
+        }
+      }
+
+      // Capture container data
+      if ($container.length > 0) {
+        event.container.id = $container.data('aa-id');
+        event.container.name = $container.data('aa-name');
+        event.container.category = $container.data('aa-category');
+        event.container.location = $container.data('aa-location');
+        event.container.product = $container.data('aa-product');
+        event.container.category = $container.data('aa-category');
+        topics = $container.data('aa-topics');
+        if (topics) {
+          event.container.topics = topics.split(',');
+        }
+        if ($container.data('aa-tags')) {
+          tags = $container.data('aa-tags').split(',');
+          tags.forEach(function (el) {
+            keyValue = el.split('::');
+            event.container.tags.push({
+              "name": keyValue[0],
+              "value": keyValue[1]
+            })
+          })
+        }
+      }
     }
 
-    // Capture component data
-    event.component.id = $component.data('aa-id');
-    event.component.name = $component.data('aa-name');
-    event.component.location = $component.data('aa-location');
-    if (!event.parentID) {
-      event.parentID = event.component.id;
-    }
+    if (event.type = 'dc') {
 
-    // Capture container data
-    event.container.id = $container.data('aa-id');
-    event.container.name = $container.data('aa-name');
-    event.container.category = $container.data('aa-category');
-    event.container.location = $container.data('aa-location');
-    event.container.product = $container.data('aa-product');
-    event.container.category = $container.data('aa-category');
-    topics = $container.data('aa-topics');
-    if (topics) {
-      event.container.topics = topics.split(',');
     }
-    if ($container.data('aa-tags')) {
-      tags = $container.data('aa-tags').split(',');
-      tags.forEach(function (el) {
-        keyValue = el.split('::');
-        event.container.tags.push({
-          "name": keyValue[0],
-          "value": keyValue[1]
-        })
-      })
-    }
-
     window.digitalData.events.push(event);
     console.log(window.digitalData.events);
   }
 
   init();
+
+
 })(jQuery)
