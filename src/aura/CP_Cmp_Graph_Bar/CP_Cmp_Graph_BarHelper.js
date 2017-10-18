645968bb5680f8a789e2cbd5cdb0d1177d4badc2
Dennis Erny <erny.dennis@gmail.com>
2017-10-18 14:47:30 -0400

    Final edit-notification changes

--- src/aura/CP_Cmp_Graph_Bar/CP_Cmp_Graph_BarHelper.js
+++ src/aura/CP_Cmp_Graph_Bar/CP_Cmp_Graph_BarHelper.js
@@ -39,7 +39,16 @@
 
 			var
 				ctx = document.getElementById("bar-chart").getContext('2d'),
-				bar;
+				bar,
+				darkBlue = "#1d5076",
+				lightBlue = "#aed8ff",
+				grey = "#d2dce4",
+				lightGreen = "#e0f1f0",
+				orange = "#fd8424",
+				seafoamGreen = "#4dede7",
+				darkPink = "#ee7481",
+				lighterBlue = "#dbfbfa",
+				darkGrey = "#cccccc";
 
 			console.warn("generateGraph");
 			console.log(ctx);
@@ -47,30 +56,22 @@
 			bar = new Chart(ctx, {
 				type: 'bar',
 				data: {
-					labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+					labels: ["YTD", "1yr", "3yr", "5yr", "Since"],
 					datasets: [{
-						label: '# of Votes',
-						data: [12, 19, 3, 5, 2, 3],
-						backgroundColor: [
-							'rgba(255, 99, 132, 0.2)',
-							'rgba(54, 162, 235, 0.2)',
-							'rgba(255, 206, 86, 0.2)',
-							'rgba(75, 192, 192, 0.2)',
-							'rgba(153, 102, 255, 0.2)',
-							'rgba(255, 159, 64, 0.2)'
-						],
-						borderColor: [
-							'rgba(255,99,132,1)',
-							'rgba(54, 162, 235, 1)',
-							'rgba(255, 206, 86, 1)',
-							'rgba(75, 192, 192, 1)',
-							'rgba(153, 102, 255, 1)',
-							'rgba(255, 159, 64, 1)'
-						],
-						borderWidth: 1
-					}]
+							label: "Harpo",
+							backgroundColor: darkBlue,
+							data: [10, 40, 35, 79, 10]
+						},
+						{
+							label: "Chico",
+							backgroundColor: seafoamGreen,
+							data: [44, 33, 55, 66, 77]
+						}
+					]
 				},
 				options: {
+					barThickness: 10,
+					maxBarThickness: 10,
 					scales: {
 						yAxes: [{
 							ticks: {
