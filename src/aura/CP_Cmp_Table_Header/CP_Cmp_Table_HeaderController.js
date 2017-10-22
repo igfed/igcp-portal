a00815b350c7fcfc51a59fdba149afcac1d89fd8
David Boyle <spektraldevelopment@Davids-MacBook-Pro.local>
2017-10-22 19:23:40 -0400

    [master] - Added back CP_Cmp_Table_Header back to repo.

---
+++ src/aura/CP_Cmp_Table_Header/CP_Cmp_Table_HeaderController.js
@@ -0,0 +1,35 @@
+({
+	onInit : function(cmp, evt, hlpr) {
+		var
+			data = cmp.get("v.headers"),
+			utils = cmp.find("CP_Utils"),
+			thClass = "",
+			style = cmp.get("v.style");
+
+		data.forEach(function(item, i) {
+
+			thClass = "igcp-table__header slds-p-bottom--x-small slds-p-horizontal--xx-small ";
+
+			if(i === (data.length - 1)) {
+				thClass += "igcp-utils__text-align--right ";
+			}
+
+			if(style.grid[i] !== null || style.grid[i] === undefined) {
+				thClass += "slds-size_" + style.grid[i] + "-of-12 ";
+			}
+
+			utils.createComponent(
+				"aura:html",
+				{ 
+					"tag": "th", 
+					"body": item,
+					"HTMLAttributes" : {
+						"class" : thClass
+					}
+				},
+				cmp, 
+				function(ready){}
+			);
+		});
+	}
+})
