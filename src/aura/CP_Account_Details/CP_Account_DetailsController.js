184f9d3ddf5c44af3027fd10524198dd876b24b0
David Boyle <spektraldevelopment@Davids-MacBook-Pro.local>
2017-10-26 21:27:47 -0400

    [master] - Applied changes for holdings to other sections in account details.

--- src/aura/CP_Account_Details/CP_Account_DetailsController.js
+++ src/aura/CP_Account_Details/CP_Account_DetailsController.js
@@ -1,5 +1,5 @@
 ({
-	onInit: function(cmp, evt, hlpr) {
+	onInit: function (cmp, evt, hlpr) {
 
 		//180030728
 		//13460563
@@ -10,7 +10,7 @@
 			utils = cmp.find("CP_Utils"),
 			events = cmp.find("CP_Events");
 
-		utils.getURLParams(function(params) {
+		utils.getURLParams(function (params) {
 			if (params.language) {
 				cmp.set("v.lang", params.language);
 			}
@@ -19,7 +19,7 @@
 		services.getAccountDetail(
 			accountNumber,
 			cmp,
-			function(success) {
+			function (success) {
 				console.log("*******");
 				console.log("Get Account Detail");
 				console.log(success);
@@ -28,7 +28,7 @@
 				cmp.set("v.accountDetailObj", success);
 
 				try {
-					utils.formatToCurrency(success.marketValueCad, function(returnedValue) {
+					utils.formatToCurrency(success.marketValueCad, function (returnedValue) {
 						cmp.set("v.marketValue", returnedValue);
 					}, cmp.get("v.lang"));
 				} catch (err) {
@@ -38,7 +38,7 @@
 				cmp.set("v.gainLossPercentage", "N/A");
 				cmp.set("v.change", "N/A");
 			},
-			function(error) {
+			function (error) {
 				console.error("Account Detail");
 				console.error(error);
 			}
@@ -47,14 +47,14 @@
 		services.getInvestmentProfile(
 			accountNumber,
 			cmp,
-			function(success) {
+			function (success) {
 				console.log("*******");
 				console.log("Get Investment Profile");
 				console.log(success);
 				console.log("*******");
 				cmp.set("v.investmentProfileObj", success);
 			},
-			function(error) {
+			function (error) {
 				console.error("Investment Profile");
 				console.error(error);
 			}
@@ -63,7 +63,7 @@
 		services.getHoldings(
 			accountNumber,
 			cmp,
-			function(success) {
+			function (success) {
 				console.log("############");
 				console.log("Get Holdings");
 				console.log(success);
@@ -74,7 +74,7 @@
 					dataArr = [],
 					dataObjArr = [];
 
-				holdingsArr.forEach(function(item, i) {
+				holdingsArr.forEach(function (item, i) {
 					dataArr.push([item.productName, item.holdingNumber, "N/A", "N/A", item.marketValueCad]);
 					dataObjArr.push(item);
 
@@ -83,95 +83,115 @@
 				events.fire(
 					"CP_Evt_Set_Table", {
 						"id": "holdings-table",
-						"headers": ["Name", "Holding", "Book Cost", "Gain / Loss", "Market Value"],
+						"headers": [
+							$A.get("$Label.c.CP_Generic_Label_Name"), 
+							$A.get("$Label.c.CP_Generic_Label_Holding"), 
+							$A.get("$Label.c.CP_Generic_Label_Book_Cost"), 
+							$A.get("$Label.c.CP_Generic_Label_Change"), 
+							$A.get("$Label.c.CP_Generic_Label_Market_Value")
+						],
 						"data": dataArr,
 						"dataObj": dataObjArr
 					}
 				);
-
-
 			},
-			function(error) {
+			function (error) {
 				console.error("Get Holdings");
 				console.error(error);
 			}
 		);
 
-		// events.fire(
-		// 	"CP_Evt_Set_Table", {
-		// 		"id": "holdings-table",
-		// 		"headers": ["Name", "Holding", "Book Cost", "Gain / Loss", "Market Value"],
-		// 		"data": [
-		// 			["Account 1", "32.7", "15034.56", "5.3", "18000.67"],
-		// 			["Account 2", "34.3", "1214.56", "5.6", "124420.67"],
-		// 			["Account 3", "14.7", "15034.56", "4.3", "5400.67"]
-		// 		]
-		// 	}
-		// );
-
 		services.getTransactions(
 			accountNumber,
 			cmp,
-			function(success) {
+			function (success) {
 				console.log("*******");
 				console.log("Get Transactions");
 				console.log(success);
 				console.log("*******");
-			},
-			function(error) {
-				console.error("Transactions");
-				console.error(error);
+
+				var
+					transactionsArr = success,
+					dataArr = [],
+					dataObjArr = [];
+
+				transactionsArr.forEach(function (item, i) {
+					dataArr.push([]);
+					dataObjArr.push(item);
+				});
+
 				events.fire(
 					"CP_Evt_Set_Table", {
 						"id": "transactions-table",
-						"headers": ["Date", "Name", "Activity", "Unit Price", "Quantity", "Amount"],
-						"data": [
-							["April 13, 2017", "Account 1", "Buy", "14.56", "7.03", "1000002.12"],
-							["April 13, 2017", "Account 2", "Sell", "14.56", "7.03", "102.12"],
-							["April 13, 2017", "Account 3", "Buy", "14.56", "7.03", "102.12"],
-							["November 13, 2017", "Account 4", "Income", "14.56", "7.03", "102.12"],
-						]
+						"headers": [
+							$A.get("$Label.c.CP_Generic_Label_Date"), 
+							$A.get("$Label.c.CP_Generic_Label_Name"), 
+							$A.get("$Label.c.CP_Generic_Label_Activity"), 
+							$A.get("$Label.c.CP_Generic_Label_Unit_Price"), 
+							$A.get("$Label.c.CP_Generic_Label_Quantity"),
+							$A.get("$Label.c.CP_Generic_Label_Amount")
+						],
+						"data": dataArr,
+						"dataObj": dataObjArr
 					}
 				);
+			},
+			function (error) {
+				console.error("Transactions");
+				console.error(error);
 			}
 		);
 
 		services.getInstructions(
 			accountNumber,
 			cmp,
-			function(success) {
+			function (success) {
 				console.log("*******");
 				console.log("Get Instructions");
 				console.log(success);
 				console.log("*******");
+
+				var
+				instructionsArr = success,
+				dataArr = [],
+				dataObjArr = [];
+
+			instructionsArr.forEach(function (item, i) {
+				dataArr.push([]);
+				dataObjArr.push(item);
+			});
+
+			events.fire(
+				"CP_Evt_Set_Table", {
+					"id": "instructions-table",
+					"headers": [
+						$A.get("$Label.c.CP_Generic_Label_Date"), 
+						$A.get("$Label.c.CP_Generic_Label_Name"), 
+						$A.get("$Label.c.CP_Generic_Label_Activity"), 
+						$A.get("$Label.c.CP_Generic_Label_Frequency"),
+						$A.get("$Label.c.CP_Generic_Label_Amount")
+					],
+					"data": dataArr,
+					"dataObj": dataObjArr
+				}
+			);
 			},
-			function(error) {
+			function (error) {
 				console.error("Instructions");
 				console.error(error);
-				events.fire(
-					"CP_Evt_Set_Table", {
-						"id": "instructions-table",
-						"headers": ["Date", "Name", "Activity", "Frequency", "Amount"],
-						"data": [
-							["April 13, 2017", "IG Mackenzie Growth Equity", "Pre-Authorized Contribution", "Monthly", "107.03"],
-							["July 13, 2017", "Growth", "Systematic Withdrawal", "Weekly", "107.03"],
-							["April 13, 2017", "IG Mackenzie Growth Equity", "Pre-Authorized Contribution", "Monthly", "107.03"]
-						]
-					}
-				);
 			}
 		);
 
 		services.getAccountPerformance(
 			accountNumber,
 			cmp,
-			function(success) {
+			function (success) {
 				console.log("*******");
 				console.log("Get Account Performance");
 				console.log(success);
 				console.log("*******");
 			},
-			function(error) {
+			function (error) {
 				console.error("Account Performance");
 				console.error(error);
 			}
@@ -181,33 +201,33 @@
 			"CP_Evt_Set_List", {
 				"id": "account-list",
 				"values": [{
-						label: 'Balance Date',
-						detail: 'Apr 13, 2017'
-					},
-					{
-						label: 'Book Cost',
-						detail: '$153,954.57'
-					},
-					{
-						label: 'YTD Contribution',
-						detail: '$3,500.00'
-					},
-					{
-						label: 'RESP Benificiary Name',
-						detail: 'Jamie Holmes'
-					},
-					{
-						label: 'Net Contributions',
-						detail: '$10,393.43'
-					},
-					{
-						label: '2016 Contributions',
-						detail: '$1,594.25'
-					},
-					{
-						label: '2017 Contributions',
-						detail: '$6,430.00'
-					}
+					label: $A.get("$Label.c.CP_Generic_Label_Balance_Date"),
+					detail: 'Apr 13, 2017'
+				},
+				{
+					label: $A.get("$Label.c.CP_Generic_Label_Book_Cost"),
+					detail: '$153,954.57'
+				},
+				{
+					label: $A.get("$Label.c.CP_Generic_Label_YTD_Contribution"),
+					detail: '$3,500.00'
+				},
+				{
+					label: $A.get("$Label.c.CP_Generic_Label_RESP_Benificiary_Name"),
+					detail: 'Jamie Holmes'
+				},
+				{
+					label: $A.get("$Label.c.CP_Generic_Label_Net_Contributions"),
+					detail: '$10,393.43'
+				},
+				{
+					label: $A.get("$Label.c.CP_Generic_Label_2016_Contributions"),
+					detail: '$1,594.25'
+				},
+				{
+					label: $A.get("$Label.c.CP_Generic_Label_2017_Contributions"),
+					detail: '$6,430.00'
+				}
 				]
 			}
 		);
@@ -217,7 +237,7 @@
 				"id": "account-details-performance-chart"
 			});
 	},
-	doneRendering: function(cmp, evt, hlpr) {
+	doneRendering: function (cmp, evt, hlpr) {
 		var body = document.querySelector("body");
 		body.className = "igcp-background__tiled igcp-utils__display--block";
 	}
