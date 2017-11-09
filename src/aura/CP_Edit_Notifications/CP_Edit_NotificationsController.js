c2e8d39b7510c07b6f08b8d7b306458ab4f469f6
David Boyle <spektraldevelopment@Davids-MacBook-Pro.local>
2017-11-09 11:34:39 -0500

    [master] - Various bug fixes.

--- src/aura/CP_Edit_Notifications/CP_Edit_NotificationsController.js
+++ src/aura/CP_Edit_Notifications/CP_Edit_NotificationsController.js
@@ -2,5 +2,31 @@
 	doneRendering: function(cmp, evt, hlpr) {
 		var body = document.querySelector("body");
 		body.className = "igcp-utils__display--block";
+	},
+	onSubmit: function(cmp, evt, hlpr){
+		
+		var services = cmp.find("CP_Services"),
+			formData = null;
+
+		formData = JSON.stringify({
+			"username" : "portalclient2@igext", 
+			"newEmail": "test1@example.com",
+			"currentEmail": "test1@example.com"
+		});
+
+		console.log(formData)
+
+		services.emailChange(
+			formData,
+			cmp,
+			function (success) {
+				console.log('[CP_Edit_Notification] - onSubmit / emailChange():success = ', success)
+			},
+			function (error) {
+				console.log('[CP_Edit_Notification] - onSubmit / emailChange():Error = ', error)
+			}
+		);
+
+		console.log('submit edit notifications')
 	}
 })
