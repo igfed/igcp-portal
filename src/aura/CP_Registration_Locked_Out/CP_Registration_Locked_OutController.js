b204b0c52545f24a91d10e8af9041e5445e5a4e5
Dennis Erny <erny.dennis@gmail.com>
2017-11-13 13:51:51 -0500

    Analytics for error pages working

--- src/aura/CP_Registration_Locked_Out/CP_Registration_Locked_OutController.js
+++ src/aura/CP_Registration_Locked_Out/CP_Registration_Locked_OutController.js
@@ -1,16 +1,5 @@
 ({
 	gotoLogin: function (cmp, evt, hlpr) {
 		cmp.find("CP_Utils").gotoLogin();
-	},
-	doneRendering: function (cmp, event, helper) {
-		if (!cmp.get("v.isDoneRendering")) {
-			cmp.set("v.isDoneRendering", true);
-			window.digitalData.error = {
-				"type": "server",
-				"code": "500",
-				"description": "Registration Locked Out"
-			};
-			window._satellite.track('saleforce-error');
-		}
 	}
 })
