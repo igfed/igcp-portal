0e6bdfb7ba3ea40219a8537b93ebf87010e34aec
Dennis Erny <erny.dennis@gmail.com>
2017-11-15 14:55:18 -0500

    Final analytics changes

--- src/aura/CP_User_Info/CP_User_InfoController.js
+++ src/aura/CP_User_Info/CP_User_InfoController.js
@@ -1,30 +1,44 @@
 ({
-	onInit: function(cmp, evt, hlpr) {
+	onInit: function (cmp, evt, hlpr) {
 		var services = cmp.find("CP_Services");
 
 		services.getUserInfo(
 			cmp,
-			function(success) {
+			function (success) {
 				cmp.set("v.userInfo", success);
-				console.log("******************");
-				console.log("USER INFO: ");
-				console.log(success);
-				console.log("******************");
+
+				// Check if user already logged in
+				var loggedIn = sessionStorage.getItem('igcp_loggedIn')
+				if (!loggedIn) {
+					setCookies();
+				}
 			},
-			function(error) {
+			function (error) {
 				console.error(error);
 			}
 		);
+
+		function setCookies() {
+			var date = new Date()
+			sessionStorage.setItem('igcp_loggedIn', true);	
+			localStorage.setItem('igcp_lastLogin', date);
+			var analytics = setInterval(function () {
+				if (window) {
+					window._aa.track('signin-start', '{"component": {"name": "CP_User_InfoController"}}');
+					clearInterval(analytics);
+				}
+			}, 500);
+		}
 	},
-	doneRendering: function(cmp, evt, hlpr) {
+	doneRendering: function (cmp, evt, hlpr) {
 		if (cmp.get("v.renderComplete") === false) {
 
 			var utils = cmp.find("CP_Utils");
 
-			utils.waitForDefined(cmp, "v.userInfo", function(userInfo) {
-				utils.forEach(userInfo, function(key, val) {
-					hlpr.waitForJquery(function(){
-						$('head').append("<meta name=" + 'igcp:' + key + " content=" + val + ">");	
+			utils.waitForDefined(cmp, "v.userInfo", function (userInfo) {
+				utils.forEach(userInfo, function (key, val) {
+					hlpr.waitForJquery(function () {
+						sessionStorage.setItem('igcp_' + key, val)
 					});
 				});
 			});
