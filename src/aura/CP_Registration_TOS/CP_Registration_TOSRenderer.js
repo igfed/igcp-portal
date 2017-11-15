dd964ea39c3c77f9b69696385094cba87d12e9cb
David Boyle <spektraldevelopment@davids-mbp.mackenziefinancial.bz>
2017-11-15 15:16:54 -0500

    [master] - Accessibility updates to registration.

--- src/aura/CP_Registration_TOS/CP_Registration_TOSRenderer.js
+++ src/aura/CP_Registration_TOS/CP_Registration_TOSRenderer.js
@@ -2,10 +2,17 @@
 	render: function (cmp, hlpr) {
 		var ret = this.superRender();
 		try {
-			window._aa.registerHandlers();
+			var body = document.querySelector("body");
+			body.className = "igcp-utils__display--block";
 		} catch (err) {
 			console.error(err);
 		}
+
+		// try {
+		// 	window._aa.registerHandlers();
+		// } catch(err) {
+		// 	console.error(err);
+		// }
 		return ret;
 	},
 	rerender: function (cmp, hlpr) {}
