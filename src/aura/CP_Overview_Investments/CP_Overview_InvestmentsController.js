cd15ed6505fcee4c7415b473edc432259c5430e6
David Boyle <spektraldevelopment@davids-mbp.mackenziefinancial.bz>
2017-11-20 14:06:18 -0500

    [master] - Hooked up new investment preview service to overview page.

--- src/aura/CP_Overview_Investments/CP_Overview_InvestmentsController.js
+++ src/aura/CP_Overview_Investments/CP_Overview_InvestmentsController.js
@@ -6,100 +6,65 @@
 			utils = cmp.find("CP_Utils");
 
 		try {
-		//GET INVESTMENT PREVIEW
-		services.getInvestmentsPreview(
-			cmp,
-			function (previewObj) {
+			//GET INVESTMENT PREVIEW
+			services.getInvestmentsPreview(
+				cmp,
+				function (previewObj) {
 
-				var
-					accountPreviewsObj = previewObj.previewAggregatesByTypeAndReg,
-					accountTypeArr = [];
+					utils.convertToMDY(previewObj.asOfDate, function (obj) {
+						cmp.set("v.asOfDate", obj.formattedString);
+					});
 
-				utils.convertToMDY(previewObj.asOfDate, function (obj) {
-					cmp.set("v.asOfDate", obj.formattedString);
-				});
+					utils.formatToCurrency(previewObj.totalValue, function (formattedValue) {
+						cmp.set("v.totalValue", formattedValue);
+					}, cmp.get("v.lang"));
 
-				utils.formatToCurrency(previewObj.totalValue, function (formattedValue) {
-					cmp.set("v.totalValue", formattedValue);
-				}, cmp.get("v.lang"));
+					//FOR NOW WE ARE COMMENTING OUT TOTAL LOSS/GAIN BECAUSE IT IS NOT READY FOR UAT
+					// utils.formatToCurrency(previewObj.totalGainLoss, function (formattedValue) {
+					// 	cmp.set("v.totalGainLoss", formattedValue);
+					// }, cmp.get("v.lang"));
 
-				//FOR NOW WE ARE COMMENTING OUT TOTAL LOSS/GAIN BECAUSE IT IS NOT READY FOR UAT
-				// utils.formatToCurrency(previewObj.totalGainLoss, function (formattedValue) {
-				// 	cmp.set("v.totalGainLoss", formattedValue);
-				// }, cmp.get("v.lang"));
+					//cmp.set("v.totalGainLossPercentage", previewObj.percGainLoss);
+					//cmp.set("v.gainLossType", previewObj.signGainLoss);
+				},
+				function (error) {
+					console.error("GET INVESTMENT PREVIEW");
+					console.error(error);
+				}
+			);
+		} catch (err) {
+			console.error('getInvestmentsPreview');
+			console.error(err)
+		}
 
-				//cmp.set("v.totalGainLossPercentage", previewObj.percGainLoss);
-				//cmp.set("v.gainLossType", previewObj.signGainLoss);
+		//GET INVESTMENT ACCOUNTS
+		try {
+			services.getInvestmentAccounts(
+				cmp,
+				function(previewObj) {
+					// console.log("Get Investment Accounts");
+					// console.info(previewObj);
+					// console.log(JSON.parse(previewObj));
 
-				//cycle through account types
-				//Group them according to dealername and registration type
-				utils.forEachSort(accountPreviewsObj, function (key, value) {
+					utils.forEach(JSON.parse(previewObj), function(key, value){
+						hlpr.addAccounts(key, value, cmp);
+					})
 
-					accountTypeArr.push(value);
+					cmp.find("CP_Events").fire("CP_Evt_Loading_Hide", {
+						"id": "overview-investments-spinner"
+					});
 
-					// try {
-					// 	services.getInvestmentsPreviewRegistered(
-					// 		key,
-					// 		cmp,
-					// 		function (success) {
-					// 			try {
+				},
+				function(error) {
+					console.error("CP_Overview: getInvestmentAccounts");
+					console.error(error);
+				}
+			);
+		} catch (err) {
+			console.error("Get Investment Accounts");
+			console.error(err)
+		}
 
-					// 				if (success !== null) {
-					// 					var safeDealerName = success.dealerName.split(" ").join("_").split(".").join("");
-					// 					sortedAccountObj[safeDealerName] = { "REGISTERED" : success.previewItems };
-					// 				}
-					// 			} catch (err) {
-					// 				console.error(err);
-					// 			}
-					// 		},
-					// 		function (error) {
-					// 			console.error(error);
-					// 		}
-					// 	);
-					// } catch (err) {
-					// 	console.error(err);
-					// }
-
-					// try {
-					// 	services.getInvestmentsPreviewNonRegistered(
-					// 		key,
-					// 		cmp,
-					// 		function (success) {
-					// 			try {
-					// 				if (success !== null) {
-					// 					var safeDealerName = success.dealerName.split(" ").join("_").split(".").join("");
-
-					// 					sortedAccountObj[safeDealerName]["UNREGISTERED"] = success.previewItems;
-					// 				}
-					// 			} catch (err) {
-					// 				console.error(err);
-					// 			}
-					// 		},
-					// 		function (error) {
-					// 			console.error(error);
-					// 		}
-					// 	);
-					// } catch (err) {
-					// 	console.error(err);
-					// }
-				});
-
-				hlpr.addAccounts(accountTypeArr, cmp);
-
-				cmp.find("CP_Events").fire("CP_Evt_Loading_Hide", {
-					"id": "overview-investments-spinner"
-				});
-
-			},
-			function (error) {
-				console.error("GET INVESTMENT PREVIEW");
-				console.error(error);
-			}
-		);
-	}catch(err) {
-		console.error('getInvestmentsPreview');
-		console.error(err)
-	}
 
 		//GET ASSET MIX
 		services.getAssetMix(
@@ -107,30 +72,30 @@
 			function (previewObj) {
 
 				var graphArr = [{
-					"label": $A.get("$Label.c.CP_Generic_Label_Cash"),
-					"detail": previewObj.totalCashAmount
+						"label": $A.get("$Label.c.CP_Generic_Label_Cash"),
+						"detail": previewObj.totalCashAmount
 
-				},
-				{
-					"label": $A.get("$Label.c.CP_Generic_Label_Fixed_Income"),
-					"detail": previewObj.totalFixedIncomeAmount
+					},
+					{
+						"label": $A.get("$Label.c.CP_Generic_Label_Fixed_Income"),
+						"detail": previewObj.totalFixedIncomeAmount
 
-				},
-				{
-					"label": $A.get("$Label.c.CP_Generic_Label_Balanced"),
-					"detail": previewObj.totalBalancedAmount
+					},
+					{
+						"label": $A.get("$Label.c.CP_Generic_Label_Balanced"),
+						"detail": previewObj.totalBalancedAmount
 
-				},
-				{
-					"label": $A.get("$Label.c.CP_Generic_Label_Equity"),
-					"detail": previewObj.totalEquityAmount
+					},
+					{
+						"label": $A.get("$Label.c.CP_Generic_Label_Equity"),
+						"detail": previewObj.totalEquityAmount
 
-				},
-				{
-					"label": $A.get("$Label.c.CP_Generic_Label_Specialty"),
-					"detail": previewObj.totalSpecialtyAmount
+					},
+					{
+						"label": $A.get("$Label.c.CP_Generic_Label_Specialty"),
+						"detail": previewObj.totalSpecialtyAmount
 
-				}
+					}
 				];
 
 				var events = cmp.find("CP_Events");
--- src/aura/CP_Overview_Investments/CP_Overview_InvestmentsHelper.js
+++ src/aura/CP_Overview_Investments/CP_Overview_InvestmentsHelper.js
@@ -1,32 +1,25 @@
 ({
-	addAccounts: function(accArr, cmp) {
-		accArr.forEach(function(item) {
+	addAccounts: function (title, accounts, cmp) {
 
-			var 
-				groupTitle = "", 
-				accountGrandTotal,
-				groupName = item[Object.keys(item)[0]].dealerName; 
+		// console.info(title);
+		// console.log(accounts);
+		var dealerName = "";
 
-			if (groupName === "Investors Group Securities Inc.") {
-				groupTitle = $A.get("$Label.c.CP_Overview_IGSI");
-			} else if (groupName === "Investors Group Financial Services Inc.") {
-				groupTitle = $A.get("$Label.c.CP_Overview_IGFS");
-			} else if (groupName === "NON IG") {
-				groupTitle = $A.get("$Label.c.CP_Overview_NON_IG");
-			}
+		if (title === "Investors Group Securities Inc.") {
+			dealerName = $A.get("$Label.c.CP_Overview_IGSI");
+		} else if (title === "Investors Group Financial Services Inc.") {
+			dealerName = $A.get("$Label.c.CP_Overview_IGFS");
+		} else if (title === "NON IG") {
+			dealerName = $A.get("$Label.c.CP_Overview_NON_IG");
+		}
 
-			//Get Dealer Name
-			cmp.find("CP_Utils").createComponent(
-				"CP_Overview_Account_Group",
-				{
-					"groupTitle" : groupTitle,
-					"accountGrandTotal" : accountGrandTotal,
-					"accounts" : item
-				},
-				cmp,
-				function(success){}
-			);
-		});
-	},
-	
+		cmp.find("CP_Utils").createComponent(
+			"CP_Overview_Account_Group", {
+				"groupTitle": dealerName,
+				"accounts": accounts
+			},
+			cmp,
+			function (success) {}
+		);
+	}
 })
