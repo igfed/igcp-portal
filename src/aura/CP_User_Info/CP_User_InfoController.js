06a330e0fb4f3530bccca3c0d73cacda0557894a
Dennis Erny <erny.dennis@gmail.com>
2017-11-20 13:38:31 -0500

    Moved UserInfo into Overview component

--- src/aura/CP_User_Info/CP_User_InfoController.js
+++ src/aura/CP_User_Info/CP_User_InfoController.js
@@ -1,44 +1,47 @@
 ({
 	onInit: function (cmp, evt, hlpr) {
 		var services = cmp.find("CP_Services");
+		var utils = cmp.find("CP_Utils");
 
-		services.getUserInfo(
-			cmp,
-			function (success) {
-				cmp.set("v.userInfo", success);
+		// Check if user already logged in
+		var loggedIn = sessionStorage.getItem('igcp_loggedIn')
+		if (!loggedIn) {
+			services.getUserInfo(
+				cmp,
+				function (success) {
+					cmp.set("v.userInfo", success);
 
-				// Check if user already logged in
-				var loggedIn = sessionStorage.getItem('igcp_loggedIn')
-				if (!loggedIn) {
-					var date = new Date()
-					var analytics = setInterval(function () {
-						if (window && window._aa) {
-							window._aa.track('signin-start', '{"component": {"name": "CP_User_InfoController"}}');
-							clearInterval(analytics);
-						}
-					}, 500);
-				}
-
-				var utils = cmp.find("CP_Utils");
-
-				utils.waitForDefined(cmp, "v.userInfo", function (userInfo) {
-					utils.forEach(userInfo, function (key, val) {
-						hlpr.waitForJquery(function () {
-							if (!val) {
-								val = "Not Available";
-							}
-							localStorage.setItem('igcp_' + key, val)
-							console.log('key:' + key + 'val:' + val);
+					utils.waitForDefined(cmp, "v.userInfo", function (userInfo) {
+						utils.forEach(userInfo, function (key, val) {
+							hlpr.waitForJquery(function () {
+								if (!val) {
+									val = "Not Available";
+								}
+								localStorage.setItem('igcp_' + key, val)
+								console.log('key:' + key + 'val:' + val);
+							});
 						});
-					});
-					sessionStorage.setItem('igcp_loggedIn', true); 
-					localStorage.setItem('igcp_lastLogin', date);
-				});
+						var date = new Date()
+						sessionStorage.setItem('igcp_loggedIn', true);
+						localStorage.setItem('igcp_lastLogin', date);
 
-			},
-			function (error) {
-				console.error(error);
-			}
-		);
+						var analytics = setInterval(function () {
+							if (window && window._aa) {
+								window._aa.track('signin-start', '{"component": {"name": "CP_User_InfoController"}}');
+								clearInterval(analytics);
+							}
+						}, 500);
+					});
+
+				},
+				function (error) {
+					console.error(error);
+				}
+			);
+
+		}
+
+
+
 	}
 })
