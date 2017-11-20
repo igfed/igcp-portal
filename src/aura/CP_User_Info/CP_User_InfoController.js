02ae10dc620b8e252c2fdc6cd751a2da4f7d0ee9
Dennis Erny <erny.dennis@gmail.com>
2017-11-20 13:10:16 -0500

    CP_User updates

--- src/aura/CP_User_Info/CP_User_InfoController.js
+++ src/aura/CP_User_Info/CP_User_InfoController.js
@@ -10,44 +10,35 @@
 				// Check if user already logged in
 				var loggedIn = sessionStorage.getItem('igcp_loggedIn')
 				if (!loggedIn) {
-					setCookies();
+					var date = new Date()
+					var analytics = setInterval(function () {
+						if (window && window._aa) {
+							window._aa.track('signin-start', '{"component": {"name": "CP_User_InfoController"}}');
+							clearInterval(analytics);
+						}
+					}, 500);
 				}
+
+				var utils = cmp.find("CP_Utils");
+
+				utils.waitForDefined(cmp, "v.userInfo", function (userInfo) {
+					utils.forEach(userInfo, function (key, val) {
+						hlpr.waitForJquery(function () {
+							if (!val) {
+								val = "Not Available";
+							}
+							localStorage.setItem('igcp_' + key, val)
+							console.log('key:' + key + 'val:' + val);
+						});
+					});
+					sessionStorage.setItem('igcp_loggedIn', true); 
+					localStorage.setItem('igcp_lastLogin', date);
+				});
+
 			},
 			function (error) {
 				console.error(error);
 			}
 		);
-
-		function setCookies() {
-			var date = new Date()
-			sessionStorage.setItem('igcp_loggedIn', true); 
-			localStorage.setItem('igcp_lastLogin', date);
-			var analytics = setInterval(function () {
-				if (window && window._aa) {
-					window._aa.track('signin-start', '{"component": {"name": "CP_User_InfoController"}}');
-					clearInterval(analytics);
-				}
-			}, 500);
-		}
-	},
-	doneRendering: function (cmp, evt, hlpr) {
-		if (cmp.get("v.renderComplete") === false) {
-
-			var utils = cmp.find("CP_Utils");
-
-			utils.waitForDefined(cmp, "v.userInfo", function (userInfo) {
-				utils.forEach(userInfo, function (key, val) {
-					hlpr.waitForJquery(function () {
-						if (!val) {
-							val = "Not Available";
-						}
-						localStorage.setItem('igcp_' + key, val)
-						console.log('key:' + key + 'val:' + val);
-					});
-				});
-			});
-
-			cmp.set("v.renderComplete", true);
-		}
 	}
 })
