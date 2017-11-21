e506e2388fc0a3956f55ea64e1ded61efe97a145
creativebrandon <admin@cre8tivebreed.com>
2017-11-21 15:26:52 -0500

    edit notifications updates

--- src/aura/CP_Cmp_Radio_Group2/CP_Cmp_Radio_Group2Controller.js
+++ src/aura/CP_Cmp_Radio_Group2/CP_Cmp_Radio_Group2Controller.js
@@ -1,12 +1,24 @@
 ({
-	onInit: function(cmp, evt, hlpr) {
+	onInit: function (cmp, evt, hlpr) {
+		// logic to set default radio
+		var radios = cmp.get('v.radios')
 
-		if(cmp.get("v.id") === "default") {
+		console.log('CP_Cmp_radios init', radios)
+
+		if (cmp.get("v.id") === "default") {
 			console.error("CP_Cmp_Radio_Group2: A unique 'id' is required.");
 		}
 
-		if(cmp.get("v.form") === "default") {
+		if (cmp.get("v.form") === "default") {
 			console.error("CP_Cmp_Radio_Group2: Input needs to be associated with a 'form'.");
 		}
+	},
+	onChange: function (cmp, evt, hlpr) {
+		var target = evt.target;
+		var value = target.getAttribute("data-value");
+		var radios = cmp.get('v.radios')
+
+		radios.value = value;
+		console.log('radios value: ', radios.value)
 	}
 })
