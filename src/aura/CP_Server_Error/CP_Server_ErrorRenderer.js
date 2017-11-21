0f68edf9d8a19f6650373167a7eef53725be2157
Dennis Erny <erny.dennis@gmail.com>
2017-11-21 17:03:36 -0500

    Updates to CP_Server_Error

--- src/aura/CP_Server_Error/CP_Server_ErrorRenderer.js
+++ src/aura/CP_Server_Error/CP_Server_ErrorRenderer.js
@@ -1,3 +1,31 @@
 ({
-	// Your renderer method overrides go here
+	render: function (cmp, hlpr) {
+		var ret = this.superRender();
+		//console.warn('Registration was rendered.')
+		try {
+			var body = document.querySelector("body");
+			body.className = "igcp-utils__display--block";
+		} catch (err) {
+			console.error(err);
+		}
+		return ret;
+	},
+	rerender: function (cmp, hlpr) {
+		//console.warn('Registration was re-rendered');
+	},
+	afterRender: function (component, helper) {
+		this.superAfterRender();
+
+		var analytics = setInterval(function () {
+			if (window && window._aa) {
+				window.digitalData.error = {
+					"type": "server",
+					"code": "500",
+					"description": "Error Description"
+				};
+				window._satellite.track('salesforce-error');
+				clearInterval(analytics);
+			}
+		}, 500);
+	}
 })
