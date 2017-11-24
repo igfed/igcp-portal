8dc31e0546bdecb8b48e0f0c489499edcde06f50
David Boyle <spektraldevelopment@davids-mbp.mackenziefinancial.bz>
2017-11-24 15:11:03 -0500

    [master] - Disabled browser back button for overview due to ISAM bug.

--- src/aura/CP_Overview/CP_OverviewController.js
+++ src/aura/CP_Overview/CP_OverviewController.js
@@ -23,9 +23,5 @@
 		);
 
 		
-	},
-	doneRendering: function(cmp, evt, hlpr) {
-		var body = document.querySelector("body");
-		body.className = "igcp-background__tiled igcp-utils__display--block";
 	}
 })
--- src/aura/CP_Overview/CP_OverviewRenderer.js
+++ src/aura/CP_Overview/CP_OverviewRenderer.js
@@ -1,3 +1,25 @@
 ({
-	// Your renderer method overrides go here
+	render: function (cmp, hlpr) {
+		var ret = this.superRender();
+		try {
+			var body = document.querySelector("body");
+			body.className = "igcp-utils__display--block";
+		} catch (err) {
+			console.error(err);
+		}
+		return ret;
+	},
+	rerender: function (cmp, hlpr) {},
+	afterRender: function (cmp, hlpr) {
+		this.superAfterRender();
+		//Disable back button due ISAM bug
+		try {
+			history.pushState(null, null, document.URL);
+			window.addEventListener('popstate', function () {
+				history.pushState(null, null, document.URL);
+			});
+		} catch (err) {
+			console.error("CP_Overview: renderer: afterRender: Could not disable back button.")
+		}
+	}
 })
