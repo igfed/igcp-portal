20432272c49711fc0482520cd46c42a513f56418
creativebrandon <admin@cre8tivebreed.com>
2017-11-30 10:39:40 -0500

    edit address initial commit

--- src/aura/CP_Edit_Address/CP_Edit_AddressController.js
+++ src/aura/CP_Edit_Address/CP_Edit_AddressController.js
@@ -1,5 +1,23 @@
 ({
-	onInit : function(component, event, helper) {
-		console.log('edit address loaded: onInit')
+	onInit: function (cmp, evt, helpr) {
+		var isDefined = !$A.util.isUndefined(cmp.get("v.formData"));
+		var provinces = ['Alberta', 'British Columbia', 'Ontario']
+		var countries = ['Canada', 'United States']
+		var data = {
+			address: '388 Achme St. West',
+			city: 'Toronto',
+			country: 'Canada',
+			phone: '416-555-5555',
+			postal: 'M5VTVT',
+			province: 'Ontario'
+		}
+
+		if (isDefined) cmp.set("v.formData", data)
+
+		cmp.set("v.countries", countries)
+		cmp.set("v.provinces", provinces)
+	},
+	onSubmit: function (cmp, evt, helpr) {
+		var formData = JSON.stringify(cmp.get("v.formData"))
 	}
 })
